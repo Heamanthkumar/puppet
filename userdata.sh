b46fe184b01ca79aad5e1fb06bab1744c60fc735
Daniel Thornton <dthornton@mozilla.com>
2014-11-25 21:11:11 -0800

    install git repo before secrets

--- userdata.sh
+++ userdata.sh
@@ -11,15 +11,15 @@ n=1
 /bin/mkdir -p /etc/facter/facts.d
 /bin/echo 'type=webapp' > /etc/facter/facts.d/classify.txt
 
+# install git repo
+/usr/bin/git clone https://github.com/relud/puppet-demo /etc/puppet
+
 # install secrets
 /bin/mkdir -p /etc/puppet/yaml/secrets
 /bin/touch /etc/puppet/yaml/secrets/common.yaml /etc/puppet/yaml/secrets/webapp.yaml
 /bin/chmod 0600 -R /etc/puppet/yaml/secrets
 /usr/bin/aws s3 cp s3://relud-demo-$n/common.yaml /etc/puppet/yaml/secrets/common.yaml
 /usr/bin/aws s3 cp s3://relud-demo-$n/webapp.yaml /etc/puppet/yaml/secrets/webapp.yaml
-
-# install git repo
-/usr/bin/git clone https://github.com/relud/puppet-demo /etc/puppet
 
 # install dependency modules
 /usr/local/bin/facter -p -j |
